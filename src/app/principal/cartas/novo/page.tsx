f546907d267fc5b30cefd7cacce10610a0ad57bd
Eduardo Teixeira <84804301+eduardostxr@users.noreply.github.com>
2024-12-05 16:19:23 -0300

    voltei

--- src/app/principal/cartas/novo/page.tsx
+++ src/app/principal/cartas/novo/page.tsx
@@ -235,19 +235,19 @@ function NovoCarta() {
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="fisico" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Físico</label>
-            <input type="number" id="fisico"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
-              {...register("fisico")}
-            />
-          </div>
-          <div className="mb-3">
             <label htmlFor="defesa" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Defesa</label>
             <input type="number" id="defesa"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
               {...register("defesa")}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="fisico" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              Físico</label>
+            <input type="number" id="fisico"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required
+              {...register("fisico")}
             />
           </div>
           <div className="mb-3">
